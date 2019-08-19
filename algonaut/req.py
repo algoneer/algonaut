fa1104757d11efc5a5ae3d8bcd733ff9a257d3fd
Andreas Dewes <andreas.dewes@kiprotect.com>
2019-08-19 18:57:01 +0200

    Removed req.

--- algonaut/req.py
+++
@@ -1,6 +0,0 @@
-with open("requirements.txt") as input_file:
-    reqs = [
-        line.strip()
-        for line in input_file.read().split()
-        if not line.strip().startswith("#") and line.find(" req: ignore") == -1
-    ]
